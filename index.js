5a5d5137183e74fd3b469afbd4002670c7487e4d
Mario Reiley <95431039+mreiley@users.noreply.github.com>
2022-05-27 12:53:22 +0000

    Proyecto terminado

--- index.js
+++ index.js
@@ -27,75 +27,36 @@ app.get('/api/hello', function(req, res) {
 // validar el url, creamos el short_url el cual enviams en un json junto
 // al url original
 app.post('/api/shorturl/',(req,res) =>{
-  const client = new Client(); 
-  const url = new URL(req.body.url);
-  if(url) {
-    // db.get(req.body.url).then(value => {
-      // console.log(value); 
-      // if(value != null) {
-        
-        // res.json({original_url : req.body.url, short_url : value});
-        // }else {
-    
-        (async () => {
-          const urlId = Math.floor(Math.random() * 100);
-          client.set(urlId,req.body.url) 
-            .then(() => { //Validar q siempre se ejecute el then asi no se registre el valor.
-              res.json({original_url : req.body.url, short_url : urlId});    
-            });
+  const client = new Client();  const prefix = /^https?:\/\//;
+  
+  try {
+    const prefix = /^https?:\/\//;
+    if (prefix.test(req.body.url)) {
+      const url = new URL(req.body.url);  
+      const urlId = Math.floor(Math.random() * 100);
+      client.set(urlId,req.body.url) 
+        .then(() => { 
+          res.json({original_url : req.body.url, short_url : urlId});    
         });
-  
-    // }
-    // });
 
-  // (async () => {
-  //     const urlId = Math.floor(Math.random() * 100);
-  //     await client.set('3', urlId).then(() => {});
-  //   }
-  // )();
-
-  // (async ()=> {
-  //     await client.get('3').then((value)=>{
-  //       console.log(value);
-  //     });
-  //   }
-   
-  // )();
-  
-  // ( async () => {
-  //     await client.delete(94).then(() => {});
-  //   }
-  // )();
-  
-  // (async ()=> {
-  //     await client.list().then((keys)=>{
-  //       keys.forEach((e) => {
-  //         client.get(e).then((value)=>{console.log(value)});
-          
-  //       });
-  //       // for(let e of keys) {
-  //       //    client.delete(e).then(()=>{});
-  //       // };
-  //     });
-  //   }
-  // )(); 
-  
-  }else{ // TypeError [ERR_INVALID_URL]: Invalid URL: mario
-    res.json({error : 'invalid url'});
+    }else {
+      res.json({error : 'invalid url' });
+    }
+  }catch(e) { // TypeError [ERR_INVALID_URL]: Invalid URL: mario
+    res.json({error : 'invalid url' });
+    
   }
 });
 
 // Buscar en el hash y realizar el redirec
 app.use('/api/shorturl/:short_url',(req,res)=>{
   const client = new Client(); 
-  (async () => {
+  
       client.get(req.params.short_url).then((value) => {
         res.redirect(value);      
       });
-    }
     
-  )();
-
+ 
 });
 
 app.listen(port, function() {
